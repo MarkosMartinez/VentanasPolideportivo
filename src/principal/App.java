7c864794a630dc66efbe2197277765d4f5c54e63
MarkosMartinez <ikbdk@plaiaundi.net>
2023-03-06 14:25:57 +0100

    Primer commit

---
+++ src/principal/App.java
@@ -0,0 +1,17 @@
+package principal;
+
+import controlador.ControladorPrincipal;
+import vista.Principal;
+import javax.swing.JButton;
+
+public class App {
+
+	public static void main(String[] args) {
+		Principal frmPrincipal = new Principal();
+
+		/*ControladorPrincipal principalC = new ControladorPrincipal(frmPrincipal);
+		principalC.inicializar();*/
+		frmPrincipal.setVisible(true);
+		
+	}
+}
